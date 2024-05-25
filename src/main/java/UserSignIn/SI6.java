1e21ddf465a5d94ae8ac15aef074f900528a49b5
Jakub <lisu317@gmail.com>
2024-05-25 21:34:11 +0100

    update
    little update

--- src/main/java/UserSignIn/SI6.java
+++ src/main/java/UserSignIn/SI6.java
@@ -22,12 +22,20 @@ public class SI6 {
         WebElement errorPopUp = fdriver.findElement(By.className("Toastify"));
         WebElement signInButton = fdriver.findElement(By.xpath("/html/body/div/main/div/form/div[3]/button"));
 
+        System.out.println("Now I'm going to try to lock the account with 10 incorrect logins and the 11th try will be correct");
         for (int i = 0; i <10; i++){
             signInButton.click();
             Thread.sleep(2500);
             System.out.println(errorPopUp.getText());
-            Thread.sleep(2500);
+            Thread.sleep(1500);
         }
+        Thread.sleep(2500);
+        username.clear();
+        password.clear();
+        username.sendKeys("fipepok686@etopys.com");
+        password.sendKeys("Password123!");
+        signInButton.click();
+        Thread.sleep(2000);
         String title = fdriver.getCurrentUrl();
         System.out.println("User got to URL:  " + title + " So the test was:");
         fdriver.close();
