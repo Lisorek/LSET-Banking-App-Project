373375b7e588980c718c0861e6c38bdf6ffd487d
Jakub <lisu317@gmail.com>
2024-06-03 19:35:26 +0100

    Test Case

---
+++ src/main/java/TransfersTab/TT7.java
@@ -0,0 +1,59 @@
+package TransfersTab;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TT7 {
+    public static void main(String[] args) throws InterruptedException {
+        FirefoxDriver fdriver = new FirefoxDriver();
+        fdriver.get("http://lsetbank.lset.uk/");
+        fdriver.manage().window().maximize();
+
+        WebElement loginButton = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[1]"));
+        loginButton.click();
+
+        WebElement username = fdriver.findElement(By.id("username"));
+        WebElement password = fdriver.findElement(By.id("password"));
+        username.sendKeys("fipepok686@etopys.com");
+        password.sendKeys("Password123!");
+
+        WebElement signInButton = fdriver.findElement(By.xpath("/html/body/div/main/div/form/div[3]/button"));
+        signInButton.click();
+        Thread.sleep(2500);
+        fdriver.navigate().refresh();
+        Thread.sleep(2000);
+
+        WebElement myAccounts = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[2]"));
+        myAccounts.click();
+        Thread.sleep(1500);
+        WebElement transferButton = fdriver.findElement(By.xpath("/html/body/div/main/div/div/div/div[1]/div[2]/button[2]"));
+        transferButton.click();
+
+        WebElement accountNumber = fdriver.findElement(By.id("productName"));
+        WebElement sortCode = fdriver.findElement(By.id("productDescription"));
+        WebElement amountField = fdriver.findElement(By.id("productPrice"));
+        WebElement submitButton = fdriver.findElement(By.xpath("/html/body/div/main/div/div/form/button"));
+        accountNumber.sendKeys("");
+        sortCode.sendKeys("744973");
+        amountField.sendKeys("1");
+        submitButton.click();
+
+        try {
+            WebElement errorPopUp = fdriver.findElement(By.xpath("/html/body/div/main/div/div/form/div[1]/div"));
+            if (errorPopUp.getText().equals("")) {
+                System.out.println("There's no error displayed");
+                System.out.println("Test Failed");
+            } else {
+                System.out.println("After providing no data to Account number, Error displayed under it says: " + errorPopUp.getText());
+                System.out.println("Test Successful");
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("No error pop-up appeared.");
+            System.out.println("Test Failed");
+        } finally {
+            fdriver.quit();
+        }
+    }
+}
