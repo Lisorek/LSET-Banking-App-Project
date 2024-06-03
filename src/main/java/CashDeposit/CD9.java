5a49cb4f8966acc0a0c4abc19ea8565cc3822842
Jakub <lisu317@gmail.com>
2024-06-03 18:45:36 +0100

    Test Case

--- src/main/java/CashDeposit/CD9.java
+++ src/main/java/CashDeposit/CD9.java
@@ -1,11 +1,10 @@
 package CashDeposit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CD8 {
+public class CD9 {
     public static void main(String[] args) throws InterruptedException {
         FirefoxDriver fdriver = new FirefoxDriver();
         fdriver.get("http://lsetbank.lset.uk/");
@@ -25,36 +24,41 @@ public class CD8 {
         fdriver.navigate().refresh();
         Thread.sleep(2000);
 
+        WebElement myAccounts = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[2]"));
+        myAccounts.click();
+        Thread.sleep(1500);
+        WebElement balance = fdriver.findElement(By.xpath("/html/body/div/main/div/div/div/div[1]/div[1]/p[3]"));
+        String balanceBefore = balance.getText();
+        System.out.println("Balance before cash deposit is:  " + balanceBefore);
         WebElement cashDeposit = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[3]"));
         cashDeposit.click();
 
         WebElement accountNumber = fdriver.findElement(By.id("productName"));
         WebElement sortCode = fdriver.findElement(By.id("productDescription"));
         WebElement amount = fdriver.findElement(By.id("productPrice"));
-        accountNumber.sendKeys("764956070");
-        sortCode.sendKeys("6780");
-        amount.sendKeys("230");
+        accountNumber.sendKeys("20912429"); // Valid Account Number
+        sortCode.sendKeys("373675");       // Valid Sort Code
+        amount.sendKeys("600");            // Valid Amount
         Thread.sleep(1500);
 
         WebElement depositButton = fdriver.findElement(By.xpath("/html/body/div/main/div/div/form/button"));
         depositButton.click();
-        Thread.sleep(4000);
+        Thread.sleep(1500);
+        WebElement balance2 = fdriver.findElement(By.cssSelector(".main-content > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(4)"));
+        String balanceAfter = balance2.getText();
+        System.out.println("Balance after cash deposit is: " + balanceAfter);
+        Thread.sleep(2000);
 
-        // Check for the error popup
-        WebElement errorPopUp = null;
         try {
-            errorPopUp = fdriver.findElement(By.className("Toastify"));
-        } catch (NoSuchElementException e) {
-            // Do nothing, errorPopUp will remain null
+            if (balanceBefore.equals(balanceAfter)) {
+                System.out.println("Balance of the account is the same now as it was on the beginning of this code");
+                System.out.println("Test Failed");
+            } else {
+                System.out.println("Cash deposit works, balance differs from what it was on the beginning");
+                System.out.println("Test Successful");
+            }
+        } finally {
+            fdriver.quit();
         }
-
-        if (errorPopUp != null && errorPopUp.isDisplayed()) {
-            System.out.println("Error displayed: " + errorPopUp.getText());
-            System.out.println("Test Successful");
-        } else {
-            System.out.println("No error pop-up appeared");
-            System.out.println("Test Failed");
-        }
-        fdriver.quit();
     }
 }
--- src/main/java/MyAccountsTab/MA2.java
+++ src/main/java/MyAccountsTab/MA2.java
@@ -1,11 +1,11 @@
-package CashDeposit;
+package MyAccountsTab;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CD7 {
+public class MA2 {
     public static void main(String[] args) throws InterruptedException {
         FirefoxDriver fdriver = new FirefoxDriver();
         fdriver.get("http://lsetbank.lset.uk/");
@@ -25,22 +25,26 @@ public class CD7 {
         fdriver.navigate().refresh();
         Thread.sleep(2000);
 
-        WebElement cashDeposit = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[3]"));
-        cashDeposit.click();
-        WebElement depositButton = fdriver.findElement(By.xpath("/html/body/div/main/div/div/form/button"));
-        depositButton.click();
         try {
-            WebElement errorPopUp = fdriver.findElement(By.className("invalid-feedback"));
-            if (errorPopUp.isDisplayed()) {
-                System.out.println("After providing no data, Error displayed says: " + errorPopUp.getText());
+            WebElement myAccounts = fdriver.findElement(By.xpath("/html/body/div/nav/div/div/button[2]"));
+            myAccounts.click();
+            Thread.sleep(1000);
+            WebElement viewTransactions = fdriver.findElement(By.xpath("/html/body/div/main/div/div/div/div[1]/div[2]/button[1]"));
+            viewTransactions.click();
+            if (fdriver.getCurrentUrl().equals("http://lsetbank.lset.uk/myaccounts/transactions")) {
+                System.out.println("View Transactions Tab works");
                 System.out.println("Test Successful");
+            } else {
+                System.out.println(fdriver.getCurrentUrl());
+                System.out.println("My accounts Tab doesn't work");
+                System.out.println("Test Failed");
             }
         } catch (NoSuchElementException e) {
-            System.out.println("No error pop-up appeared. All the fields were empty");
-            System.out.println("Test Failed");
+            System.out.println("Test Failed -  No Element found");
         } finally {
             fdriver.quit();
         }
+
+
     }
 }
-
